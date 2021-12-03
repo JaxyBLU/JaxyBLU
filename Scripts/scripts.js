ae951193e25a24880f2c27e76926f0f710a8fc38
JaxyBLU <tukebaike123@gmail.com>
2021-12-03 22:30:20 +0400

    I added about,html file and made few more design progress

---
+++ Scripts/scripts.js
@@ -0,0 +1,10 @@
+const linkMyGitHub = 'https://github.com/JaxyBLU';
+const linkMyInstagram = 'https://www.instagram.com/joldoshoff/';
+const myEmail = 'tukebaike123@gmail.com';
+
+document.getElementById('about_title').innerHTML = `Little introduction of myself`;
+document.getElementById('about_text').innerHTML = `My name is Turdubek Zholdoshov. I'm 25 
+years old and I'm currently living and working in Dubai, UAE. I started studying Web
+Development 5 months before. I decided that learning Frontend Web Development fits best for
+me at the moment. Therefore I started to learn Frontend technologies and tools.
+You can join me in my journey and track my progress.`;
